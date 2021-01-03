98bd029e709d0c07ecd275e3d7910674933ffff3
Vageesha Krishna <53399088+vageeshakrishna@users.noreply.github.com>
2021-01-03 17:58:42 +0530

    animation stagger

--- src/com/Loginprocess.java
+++ src/com/Loginprocess.java
@@ -14,28 +14,31 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Loginprocess")
 public class Loginprocess extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Loginprocess() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	public Loginprocess() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String uid = request.getParameter("email");
 		String pwd = request.getParameter("password");
 		if (uid.equals("admin@admin") && pwd.equals("pass") || uid.equals("zod@admin") && pwd.equals("krypton")) {
